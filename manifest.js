0d3d6e3e6019848dc461f297ca51c4d07ab3fd26
DragPvP <154012086+DragPvP@users.noreply.github.com>
2024-08-20 13:08:33 +0500

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,5 +1,5 @@
 {
     "name": "Dogs",
     "url": "https://onetime.dog",
-    "icon": "",
+    "icon": "https://github.com/DragPvP/fqy3p98yqpfcohwa/blob/2b8cabe4f6051f047cc52a0049aee15ceafeaefa/Dog.png",
 }
